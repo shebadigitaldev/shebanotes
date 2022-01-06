11c3cfeb1eacb32663c39bc321de930e6419dcca
sonalisikkha@gmail.com <sonalisikkha@gmail.com>
2022-01-06 12:06:20 +0600

    add

---
+++ precache-manifest.fcf84cffaadc8ff2037065fc98804842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0613e67807c762f05677736dfcf8abaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "745dca8fde4de8c85d57",
+    "url": "/static/css/main.b8597633.chunk.css"
+  },
+  {
+    "revision": "7178be826aa641f6fddf",
+    "url": "/static/js/2.942037dd.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/static/js/2.942037dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "745dca8fde4de8c85d57",
+    "url": "/static/js/main.ca95a4ad.chunk.js"
+  },
+  {
+    "revision": "3c346ed830fde6252f7d",
+    "url": "/static/js/runtime-main.8f09ae5e.js"
+  }
+]);
